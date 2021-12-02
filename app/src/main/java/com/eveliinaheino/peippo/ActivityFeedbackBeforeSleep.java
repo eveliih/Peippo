618a3e4a88bc47ee0996cc7e99f3f403c63826f0
“eveliih” <eveliina.heino@metropolia.fi>
2021-12-02 12:07:47 +0200

    Tietojen tallennus sharedpreferensseihin. Lisäsin myös toast viestin käyttäjälle, jos yrittää tallentaa tietoja valitsematta kaikkiin kohtiin arvoja. Tein myös eilen jotain popup ikkuna kokeiluja niin niistä voi näkyä jotain kommentoituja ylimääräisiä juttuja, mutta ne voi jättää huomioimatta. Testi aktiviteetti luokan voi poistaa halutessaan. Siinä oikeassa tietojen tarkastelu aktiviteetissa täytyy sit varmaan huomioida jotenkin se, että ekaa kertaa kun se avataan, voi olla ettei mitään tietoja ole vielä syötetty.

--- app/src/main/java/com/eveliinaheino/peippo/ActivityFeedbackBeforeSleep.java
+++ app/src/main/java/com/eveliinaheino/peippo/ActivityFeedbackBeforeSleep.java
@@ -2,20 +2,45 @@ package com.eveliinaheino.peippo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.WindowManager;
 import android.widget.TextView;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
-public class ActivityFeedbackBeforeSleep extends AppCompatActivity {
+public class ActivityFeedbackBeforeSleep extends Activity{
     ArrayList <PeippoVariables> list;
     int mood;
     int tiredness;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_before_sleep);
+        //testailin tehdä tästä popup ikkunaa, mutta yllätys yllätys siitä koitui kaikenlaisia haasteita xD Jätän nää nyt vielä tähän, jos jää aika tutkailla tätä
+       /* DisplayMetrics displayMetrics = new DisplayMetrics(); //tulevalla jutulla tehdään Activitystä popup ikkuna
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes(); //tällä asetetaan popup ikkunan sijainti
+        params.gravity = Gravity.CENTER;
+        params.x = 0; //keskellä
+        params.y = -20;
+
+        getWindow().setAttributes(params);*/
 
         TextView textViewMood = findViewById(R.id.textViewMoodFeedback);
         TextView textViewTiredness = findViewById(R.id.textViewTirednessFeedback);
@@ -29,5 +54,26 @@ public class ActivityFeedbackBeforeSleep extends AppCompatActivity {
         textViewMood.setText(messages.getMoodMessage());
         textViewTiredness.setText(messages.getTirednessMessage());
 
+
     }
+
+
+    @Override
+    protected void onStop() {
+        list = SingletonMoodsAndTiredness.getInstance().getList();
+        super.onStop();
+        //halutaanko tallentaa tiedot vasta tässä kohtaa? Nyt tallennus tapahtuu samalla, kun käyttäjä klikkaa Tallenna nappulaa ActivityAddDataBeforeSleep
+        /* Gson gson = new Gson();
+
+        String jsonPeippoVariables = gson.toJson(list);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+
+        prefEditor.putString("jsonPeippoVariables", jsonPeippoVariables);
+
+        prefEditor.commit(); */
+
+    }
+
 }
