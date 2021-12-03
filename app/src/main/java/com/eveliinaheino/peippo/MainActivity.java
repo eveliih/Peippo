fb1b719c88ac0f1d3fa2a263bdab65bfbdabb140
“eveliih” <eveliina.heino@metropolia.fi>
2021-12-03 13:52:36 +0200

    commit

--- app/src/main/java/com/eveliinaheino/peippo/MainActivity.java
+++ app/src/main/java/com/eveliinaheino/peippo/MainActivity.java
@@ -2,7 +2,9 @@ package com.eveliinaheino.peippo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i = 0;
-        i++;
+    }
+    public void buttonAddAfterSleepClicked(View view){
+        Intent intent = new Intent(this, ActivityAddDataAfterSleep.class);
+        startActivity(intent);
+    }
+    public void buttonAddBeforeSleepClicked(View view){
+        Intent intent = new Intent(this, ActivityAddDataBeforeSleep.class);
+        startActivity(intent);
+    }
+    public void buttonSeeTipsClicked(View view){
+        Intent intent = new Intent(this, ActivityTipList.class);
+        startActivity(intent);
     }
 }
