afda0dbbaa2c1efc18d30a9f19fe9b011d7f8987
“Katanpe” <katja.peltonen3@metropolia.fi>
2021-12-07 14:08:34 +0200

    Lisätty landscape orientaatio vinkeille ja singleton, josta löytyvät kaikki vinkkien tiedot

--- app/src/main/java/com/eveliinaheino/peippo/ActivityTipDetails.java
+++ app/src/main/java/com/eveliinaheino/peippo/ActivityTipDetails.java
@@ -5,19 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ActivityTipDetails extends AppCompatActivity {
+    private ArrayList<String> titles;
+    private ArrayList<String> texts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_details);
 
+        titles = SingletonTips.getInstance().getTitles();
+        texts = SingletonTips.getInstance().getTexts();
+
         Bundle b = getIntent().getExtras();
         int i = b.getInt(ActivityTipList.EXTRATIPS, 0);
 
         ((TextView)findViewById(R.id.textViewTitle))
-                .setText(ActivityTipList.getTipTitle(i));
+                .setText(titles.get(i));
         ((TextView)findViewById(R.id.textViewTip))
-                .setText(TipText.getTipText(i));
+                .setText(texts.get(i));
     }
 }
