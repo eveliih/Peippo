5f91ee9aa4e6c6fcaa76153e4b86177f97b96695
“eveliih” <eveliina.heino@metropolia.fi>
2021-12-08 17:01:37 +0200

    Ohjeet skrollattavassa popup ikkunassa. Poistin turhia omia kommentteja

--- app/src/main/java/com/eveliinaheino/peippo/ActivityFeedbackBeforeSleep.java
+++ app/src/main/java/com/eveliinaheino/peippo/ActivityFeedbackBeforeSleep.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
-public class ActivityFeedbackBeforeSleep extends Activity{
+public class ActivityFeedbackBeforeSleep extends AppCompatActivity{
     ArrayList <PeippoVariables> list;
     int mood;
     int tiredness;
@@ -16,21 +18,6 @@ public class ActivityFeedbackBeforeSleep extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_before_sleep);
-        //testailin tehdä tästä popup ikkunaa, mutta yllätys yllätys siitä koitui kaikenlaisia haasteita xD Jätän nää nyt vielä tähän, jos jää aika tutkailla tätä
-       /* DisplayMetrics displayMetrics = new DisplayMetrics(); //tulevalla jutulla tehdään Activitystä popup ikkuna
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-
-        int width = displayMetrics.widthPixels;
-        int height = displayMetrics.heightPixels;
-
-        getWindow().setLayout((int)(width*.8), (int)(height*.7));
-
-        WindowManager.LayoutParams params = getWindow().getAttributes(); //tällä asetetaan popup ikkunan sijainti
-        params.gravity = Gravity.CENTER;
-        params.x = 0; //keskellä
-        params.y = -20;
-
-        getWindow().setAttributes(params);*/
 
         TextView textViewEveningFeedback = findViewById(R.id.textViewEveningFeedback);
 
@@ -43,24 +30,4 @@ public class ActivityFeedbackBeforeSleep extends Activity{
         textViewEveningFeedback.setText(messages.getFeedbackEvening());
 
     }
-
-
-    @Override
-    protected void onStop() {
-        list = SingletonPeippoVariablesList.getInstance().getList();
-        super.onStop();
-        //halutaanko tallentaa tiedot vasta tässä kohtaa? Nyt tallennus tapahtuu samalla, kun käyttäjä klikkaa Tallenna nappulaa ActivityAddDataBeforeSleep
-        /* Gson gson = new Gson();
-
-        String jsonPeippoVariables = gson.toJson(list);
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
-
-        prefEditor.putString("jsonPeippoVariables", jsonPeippoVariables);
-
-        prefEditor.commit(); */
-
-    }
-
 }
--- app/src/main/java/com/eveliinaheino/peippo/ActivityManual.java
+++ app/src/main/java/com/eveliinaheino/peippo/ActivityManual.java
@@ -2,16 +2,35 @@ package com.eveliinaheino.peippo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 
-public class ActivityManual extends AppCompatActivity {
+public class ActivityManual extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics(); //tulevalla jutulla tehdään Activitystä popup ikkuna
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int)(width*.8), (int)(height*.5));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes(); //tällä asetetaan popup ikkunan sijainti
+        params.gravity = Gravity.CENTER;
+        params.x = 0; //keskellä
+        params.y = -10;
+
+        getWindow().setAttributes(params);
     }
     public void buttonOkClicked(View view){
         Intent intent = new Intent(this, MainActivity.class);
